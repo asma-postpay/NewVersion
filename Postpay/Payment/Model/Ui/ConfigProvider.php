3ea954135a35057fa471ecdcb704d388023524ae
asma-postpay <asma.hawari@postpay.io>
2023-05-02 15:18:22 +0400

    Modify PriceBox Logic to support Configurable Products

--- Postpay/Payment/Model/Ui/ConfigProvider.php
+++ Postpay/Payment/Model/Ui/ConfigProvider.php
@@ -60,12 +60,12 @@ class ConfigProvider implements ConfigProviderInterface
                     'inContext' => $this->config->inContext(),
                     'summaryWidget' => $this->config->summaryWidgetEnabled(),
                     'checkoutUrl' => $this->urlBuilder->getUrl('postpay/payment/checkout'),
-                    'icon' => 'https://cdn.postpay.io/imgs/postpay-new-logo.png'
+                    'icon' => 'https://cdn-dev.postpay.io/e/images/postpay-light.png'
                 ],
                 PayNowConfig::CODE => [
                     'summaryWidget' => $this->payNowConfig->summaryWidgetEnabled(),
                     'numInstalments' => PayNow::NUM_INSTALMENTS,
-                    'icon' => 'https://cdn.postpay.io/imgs/postpay-new-logo.png'
+                    'icon' => 'https://cdn-dev.postpay.io/e/images/postpay-light.png'
                 ]
             ]
         ];
